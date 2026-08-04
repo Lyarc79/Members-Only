0469cc18c9e3d667d91030042d104e35d1c36b70
Lyarc79 <141921696+Lyarc79@users.noreply.github.com>
2026-08-04 17:44:57 +0200

    fix: add missing import/export and delegate signup GET to controller

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,12 +1,13 @@
 const { Router } = require("express");
 const { validateSignup } = require("../middlewares/formsValidation");
+const indexController = require("../controllers/indexController");
 
 const indexRouter = Router();
 
 indexRouter.get("/", (req, res) => {
   res.render("index");
 });
-indexRouter.get("/signup", (req, res) => {
-  res.render("signup");
-});
+indexRouter.get("/signup", indexController.getSignupForm);
 indexRouter.post("/signup", validateSignup, indexController.postSignupForm);
+
+module.exports = indexRouter;
