fbbe5728e91890e659fb4d2afac39808573b0666
Lyarc79 <141921696+Lyarc79@users.noreply.github.com>
2026-08-04 17:39:42 +0200

    fix: add missing exports and pass user and validation errors to signup render

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -2,13 +2,27 @@ const db = require("../db/queries");
 const { validationResult } = require("express-validator");
 const bcrypt = require("bcrypt");
 
+async function getSignupForm(req, res) {
+  return res.render("signup", {
+    user: {},
+  });
+}
+
 async function postSignupForm(req, res) {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.render("signup");
+    return res.render("signup", {
+      errors: errors.array(),
+      user: req.body,
+    });
   }
   const { firstName, lastName, username, password } = req.body;
   const hashedPassword = await bcrypt.hash(req.body.password, 10);
   await db.createUser(firstName, lastName, username, hashedPassword);
   res.redirect("/login");
 }
+
+module.exports = {
+  getSignupForm,
+  postSignupForm,
+};
