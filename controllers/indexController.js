7761671aabea4e0400e27cee3a3c54f142d81e7a
Lyarc79 <141921696+Lyarc79@users.noreply.github.com>
2026-08-05 17:05:00 +0200

    feat: implement login / logout routes and controller actions

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,10 +1,11 @@
 const db = require("../db/queries");
 const { validationResult } = require("express-validator");
 const bcrypt = require("bcrypt");
+const passport = require("passport");
 
 async function getSignupForm(req, res) {
   return res.render("signup", {
-    user: {},
+    formData: {},
   });
 }
 
@@ -13,7 +14,7 @@ async function postSignupForm(req, res) {
   if (!errors.isEmpty()) {
     return res.render("signup", {
       errors: errors.array(),
-      user: req.body,
+      formData: req.body,
     });
   }
   const { firstName, lastName, username, password } = req.body;
@@ -22,7 +23,45 @@ async function postSignupForm(req, res) {
   res.redirect("/login");
 }
 
+async function getLoginForm(req, res) {
+  return res.render("login", {
+    formData: {},
+  });
+}
+
+async function postLoginForm(req, res, next) {
+  passport.authenticate("local", (err, user, info) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return res.render("login", {
+        errors: [{ msg: info ? info.message : "Invalid credentials" }],
+        formData: req.body,
+      });
+    }
+    req.login(user, (err) => {
+      if (err) {
+        return next(err);
+      }
+      return res.redirect("/");
+    });
+  })(req, res, next);
+}
+
+async function logoutUser(req, res, next) {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+}
+
 module.exports = {
   getSignupForm,
   postSignupForm,
+  getLoginForm,
+  postLoginForm,
+  logoutUser,
 };
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,13 +1,18 @@
 const { Router } = require("express");
 const { validateSignup } = require("../middlewares/formsValidation");
 const indexController = require("../controllers/indexController");
+const passport = require("passport");
+const { isGuest } = require("../middlewares/login-check");
 
 const indexRouter = Router();
 
 indexRouter.get("/", (req, res) => {
   res.render("index");
 });
-indexRouter.get("/signup", indexController.getSignupForm);
+indexRouter.get("/signup", isGuest, indexController.getSignupForm);
 indexRouter.post("/signup", validateSignup, indexController.postSignupForm);
+indexRouter.get("/login", isGuest, indexController.getLoginForm);
+indexRouter.post("/login", indexController.postLoginForm);
+indexRouter.get("/logout", indexController.logoutUser);
 
 module.exports = indexRouter;
